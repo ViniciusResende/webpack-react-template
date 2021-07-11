1cb2d179b2a5053e7348b579e3c33e7ca8858406
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-07-11 11:36:37 -0300

    Prettier configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './styles.css';
+import './styles.css'
 
-import logo from './logo512.png';
-import adobeLogo from './adobe.svg';
-import { ClickCounter } from './ClickCounter';
+import logo from './logo512.png'
+import adobeLogo from './adobe.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
   return (
@@ -11,9 +11,9 @@ export const App = () => {
         Edited React TypeScript Webpack Starter Template -{' '}
         {process.env.NODE_ENV} | {process.env.name}
       </h1>{' '}
-      <img src={logo} alt='React Logo' width='300' height='300' />
-      <img src={adobeLogo} alt='React Logo' width='300' />
+      <img src={logo} alt="React Logo" width="300" height="300" />
+      <img src={adobeLogo} alt="React Logo" width="300" />
       <ClickCounter />
     </>
-  );
-};
+  )
+}
