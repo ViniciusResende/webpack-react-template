2d5099689876427ec022f72425bcccbbd93e45f8
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-07-11 11:12:25 -0300

    Added react-refresh plugin

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import './styles.css';
 
 import logo from './logo512.png';
 import adobeLogo from './adobe.svg';
+import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
   return (
     <>
       <h1>
-        React TypeScript Webpack Starter Template - {process.env.NODE_ENV} |{' '}
-        {process.env.name}
+        Edited React TypeScript Webpack Starter Template -{' '}
+        {process.env.NODE_ENV} | {process.env.name}
       </h1>{' '}
       <img src={logo} alt='React Logo' width='300' height='300' />
       <img src={adobeLogo} alt='React Logo' width='300' />
+      <ClickCounter />
     </>
   );
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,18 @@
 const webpck = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3792,
+  },
   devtool: 'cheap-module-source-map',
   plugins: [
     new webpck.DefinePlugin({
       'process.env.name': JSON.stringify('Vishwas'),
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
 };
