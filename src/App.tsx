ced8be67be0ade9c1ba9869f52524046e8555434
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-07-11 10:38:36 -0300

    Added css, image and svg configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import './styles.css';
+
+import logo from './logo512.png';
+import adobeLogo from './adobe.svg';
+
 export const App = () => {
-  return <h1>React TypeScript Webpack Starter Template</h1>;
+  return (
+    <>
+      <h1>React TypeScript Webpack Starter Template</h1>{' '}
+      <img src={logo} alt='React Logo' width='300' height='300' />
+      <img src={adobeLogo} alt='React Logo' width='300' />
+    </>
+  );
 };
